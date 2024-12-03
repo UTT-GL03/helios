88499b5d277a9c7daab3cf959bb00795ec02fbf0
Eflii <hugo.efligenir@gmail.com>
2024-12-03 15:10:44 +0100

    Add graph element and @mui/chart

--- frontend/src/ProPage.jsx
+++ frontend/src/ProPage.jsx
@@ -1,9 +1,12 @@
 
 import './App.css';
 
+import WeatherGraph from './WeatherGraph';
+
 function ProPage() {
     return (
         <>
+          <WeatherGraph />
           <progress />
 
           <h1 >Développement en cours </h1>
---
+++ frontend/src/WeatherGraph.jsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+import { LineChart } from '@mui/x-charts/LineChart';
+
+function WeatherGraph() {
+    return (
+        <LineChart
+      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+      series={[
+        {
+          data: [2, 5.5, 2, 8.5, 1.5, 5],
+        },
+      ]}
+      width={500}
+      height={300}
+    />
+    )
+}
+
+export default WeatherGraph;
