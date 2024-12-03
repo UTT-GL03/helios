1408a4cff2d2a0aa6583a510befe6a48021eb4d1
Eflii <hugo.efligenir@gmail.com>
2024-12-03 15:40:53 +0100

    change package for chart management

--- frontend/src/WeatherGraph.jsx
+++ frontend/src/WeatherGraph.jsx
@@ -1,19 +1,11 @@
 
 import './App.css';
-import { LineChart } from '@mui/x-charts/LineChart';
+import { LineChart } from 'react-chartkick'
+import 'chartkick/chart.js'
 
 function WeatherGraph() {
     return (
-        <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-      series={[
-        {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
-        },
-      ]}
-      width={500}
-      height={300}
-    />
+        <LineChart data={{"2025-05-13": 2, "2025-05-14": 5,"2025-05-15":3 }} />
     )
 }
 
